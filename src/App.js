2512fdf5c57c5f6fd1c7f7a3909cf9e9890ba8ae
indira-faizullina <indiramus@list.ru>
2023-05-02 12:34:45 +1100

    added Colors component

--- src/App.js
+++ src/App.js
@@ -5,28 +5,39 @@ import Modal from './Modal'
 import ModalWithAnimation from './ModalWithAnimation'
 import Quiz from './Quiz'
 import UsersList from './usersList/UsersList'
+import Colors from './Colors'
 
 export default function App() {
   const miniProjects = [
     {
       title: 'Счётчик',
       id: '1',
+      component: <Counter />,
     },
     {
       title: 'Вызов модального окна',
       id: '2',
+      component: <Modal />,
     },
     {
       title: 'Вызов модального окна с плавной анимацией',
       id: '3',
+      component: <ModalWithAnimation />,
     },
     {
       title: 'Квиз',
       id: '4',
+      component: <Quiz />,
     },
     {
       title: 'Работа со списком пользователей',
       id: '5',
+      component: <UsersList />,
+    },
+    {
+      title: 'Цвета',
+      id: '6',
+      component: <Colors />,
     },
   ]
 
@@ -36,38 +47,29 @@ export default function App() {
     setProjectNumber(Number(event.target.id))
   }
 
-  switch (projectNumber) {
-    case 1:
-      return <Counter />
-      break
-    case 2:
-      return <Modal />
-      break
-    case 3:
-      return <ModalWithAnimation />
-      break
-    case 4:
-      return <Quiz />
-      break
-    case 5:
-      return <UsersList />
-      break
-    default:
-      return (
-        <div>
-          <h1>Список моих мини-проектов для изучения React</h1>
-          <ul>
-            {miniProjects.map((project) => (
-              <li
-                key={project.id}
-                id={project.id}
-                onClick={selectProjectHandler}
-              >
-                {project.title}
-              </li>
-            ))}
-          </ul>
-        </div>
-      )
+  const selectedProject = miniProjects.filter(
+    (project) => Number(project.id) === projectNumber
+  )
+
+  if (selectedProject.length === 0) {
+    return (
+      <div>
+        <h1>Список моих мини-проектов для изучения React</h1>
+        <ul>
+          {miniProjects.map((project) => (
+            <li
+              key={project.id}
+              id={project.id}
+              className="userLi"
+              onClick={selectProjectHandler}
+            >
+              {project.title}
+            </li>
+          ))}
+        </ul>
+      </div>
+    )
+  } else {
+    return selectedProject.map((project) => project.component)
   }
 }
---
+++ src/Colors.js
@@ -0,0 +1,33 @@
+import styles from './Colors.module.scss'
+import React, { useState } from 'react'
+
+export default function Colors() {
+  const [colors, setColor] = useState(['rgb(13 100 200)'])
+
+  const getRandomNumber = () => {
+    const randomNumber = Math.round(Math.random() * 255)
+    return randomNumber
+  }
+
+  const addRandomColor = () => {
+    const newColor = `rgb(${getRandomNumber()} ${getRandomNumber()} ${getRandomNumber()})`
+    setColor([...colors, newColor])
+  }
+
+  return (
+    <div>
+      <ul className={styles.ul}>
+        {colors.map((color, index) => (
+          <li
+            key={index}
+            className={styles.li}
+            style={{ background: color }}
+          ></li>
+        ))}
+      </ul>
+      <button className="btn btn-primary" onClick={addRandomColor}>
+        Добавить новый цвет
+      </button>
+    </div>
+  )
+}
